9e3630607d0c68d30cce1de1d626bb06f1dbf693
datachai-rei <96822858+datachai-rei@users.noreply.github.com>
2025-04-03 16:09:00 +0800

    Update password.js

--- password.js
+++ password.js
@@ -1,7 +1,7 @@
 const password = "lose5kgs"; 
 
 function checkPassword() {
-    let userInput = prompt("Enter Password:");
+    let userInput = prompt("パスワードを入力してください。");
     if (userInput !== password) {
         alert("パスワードが違います。");
         window.location.href = "https://datachai-rei.github.io/gohan_dousuru/gohan_dousuru_error.html"; 
